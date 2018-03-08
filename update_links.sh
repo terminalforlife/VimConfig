07b0903df6df3cec61474f40c2e8ad92af2dd713
terminalforlife <terminalforlife@yahoo.com>
2018-03-08 07:01:57 +0000

    Updated update_links.sh; optimized

--- update_links.sh
+++ update_links.sh
@@ -3,29 +3,29 @@
 #----------------------------------------------------------------------------------
 # Project Name      - miscellaneous/update_links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Mon  5 Mar 17:12:36 GMT 2018
+# Last Change       - Thu  8 Mar 06:01:03 GMT 2018
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
-#------------------------------------------------------------------------------MAIN
+#----------------------------------------------------------------------------------
 
 XERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; exit 1; }
 ERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; }
 
 declare -i DEPCOUNT=0
 for DEP in /bin/{ln,rm}; {
-	[ -x "$DEP" ] || {
+	if ! [ -x "$DEP" ]; then
 		ERR "$LINENO" "Dependency '$DEP' not met."
 		DEPCOUNT+=1
-	}
+	fi
 }
 
 [ $DEPCOUNT -eq 0 ] || exit 1
 
-[ "${PWD//*\/}" == "vimconfig" ] || {
+if ! [ "${PWD//*\/}" == "vimconfig" ]; then
 	XERR "$LINENO" "Not in the repository's root directory."
-}
+fi
 
 for FILE in plugin/*.vim; {
 	/bin/rm -v $HOME/.vim/plugin/${FILE//*\/} 2> /dev/null
