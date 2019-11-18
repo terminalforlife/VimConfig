bbe816e5aa67fca1ba15be0ebf37bda3afdabfa9
terminalforlife <terminalforlife@yahoo.com>
2019-11-18 16:19:30 +0000

    Remove absolute executable paths

--- update_links.sh
+++ update_links.sh
@@ -3,20 +3,22 @@
 #----------------------------------------------------------------------------------
 # Project Name      - VimConfig/update_links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Thu  9 May 13:56:22 BST 2019
+# Last Change       - Mon 18 Nov 16:18:50 GMT 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
 #----------------------------------------------------------------------------------
 
-XERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; exit 1; }
-ERR(){ printf "[L%0.4d] ERROR: %s\n" "$1" "$2" 1>&2; }
+FAIL(){
+	printf "[L%0.4d] ERROR: %s\n" "$2" "$3" 1>&2
+	[ $1 -eq 1 ] && exit 1
+}
 
 declare -i DEPCOUNT=0
-for DEP in /bin/{ln,rm}; {
-	if ! [ -x "$DEP" ]; then
-		ERR "$LINENO" "Dependency '$DEP' not met."
+for DEP in rm ln; {
+	if ! type -fP "$DEP" > /dev/null 2>&1; then
+		FAIL 0 "$LINENO" "Dependency '$DEP' not met."
 		DEPCOUNT+=1
 	fi
 }
@@ -24,22 +26,21 @@ for DEP in /bin/{ln,rm}; {
 [ $DEPCOUNT -eq 0 ] || exit 1
 
 if ! [ "${PWD//*\/}" == "VimConfig" ]; then
-	XERR "$LINENO" "Not in the repository's root directory."
+	FAIL 1 "$LINENO" "Not in the repository's root directory."
 fi
 
 for FILE in plugin/*.vim; {
-	/bin/rm -v $HOME/.vim/plugin/${FILE//*\/} 2>&-
-	/bin/ln -v "$FILE" $HOME/.vim/plugin/${FILE//*\/} 2>&-
+	rm -v $HOME/.vim/plugin/${FILE//*\/} 2>&-
+	ln -v "$FILE" $HOME/.vim/plugin/${FILE//*\/} 2>&-
 }
 
 for FILE in snippet/*; {
-	/bin/rm -v $HOME/.vim/snippet/${FILE//*\/} 2>&-
-	/bin/ln -v "$FILE" $HOME/.vim/snippet/${FILE//*\/} 2>&-
+	rm -v $HOME/.vim/snippet/${FILE//*\/} 2>&-
+	ln -v "$FILE" $HOME/.vim/snippet/${FILE//*\/} 2>&-
 }
 
-/bin/rm -v $HOME/.vim/colors/tfl.vim 2>&-
-/bin/ln -v colors/tfl.vim $HOME/.vim/colors/ 2>&-
+rm -v $HOME/.vim/colors/tfl.vim 2>&-
+ln -v colors/tfl.vim $HOME/.vim/colors/ 2>&-
 
-/bin/rm -v $HOME/.vimrc 2>&-
-/bin/ln -v .vimrc $HOME/ 2>&-
-
+rm -v $HOME/.vimrc 2>&-
+ln -v .vimrc $HOME/ 2>&-
