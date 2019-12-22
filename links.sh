24daf2bbf267075c8cc9c0d6a83e33e71897aa0b
terminalforlife <terminalforlife@yahoo.com>
2019-12-22 23:59:01 +0000

    `update_links.sh` > `links.sh`

--- links.sh
+++ links.sh
@@ -1,43 +1,35 @@
-#!/bin/bash
+#!/bin/sh
 
 #----------------------------------------------------------------------------------
-# Project Name      - VimConfig/update_links.sh
+# Project Name      - VimConfig/_links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Mon 18 Nov 16:18:50 GMT 2019
+# Last Change       - Sun 22 Dec 23:58:35 GMT 2019
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #----------------------------------------------------------------------------------
 # Just a simple, quick script to update the hard links when changing branches.
 #----------------------------------------------------------------------------------
 
-FAIL(){
-	printf "[L%0.4d] ERROR: %s\n" "$2" "$3" 1>&2
-	[ $1 -eq 1 ] && exit 1
-}
+set -e
+. /usr/lib/tflbp-sh/Err
+. /usr/lib/tflbp-sh/ChkDep
+set +e
 
-declare -i DEPCOUNT=0
-for DEP in rm ln; {
-	if ! type -fP "$DEP" > /dev/null 2>&1; then
-		FAIL 0 "$LINENO" "Dependency '$DEP' not met."
-		DEPCOUNT+=1
-	fi
-}
+ChkDep ln rm
 
-[ $DEPCOUNT -eq 0 ] || exit 1
-
-if ! [ "${PWD//*\/}" == "VimConfig" ]; then
-	FAIL 1 "$LINENO" "Not in the repository's root directory."
+if ! [ "${PWD##*/}" == 'VimConfig' ]; then
+	Err 1 "Not in the repository's root directory."
 fi
 
-for FILE in plugin/*.vim; {
+for FILE in plugin/*.vim; do
 	rm -v $HOME/.vim/plugin/${FILE//*\/} 2> /dev/null
 	ln -v "$FILE" $HOME/.vim/plugin/${FILE//*\/} 2> /dev/null
-}
+done
 
-for FILE in snippet/*; {
+for FILE in snippet/*; do
 	rm -v $HOME/.vim/snippet/${FILE//*\/} 2> /dev/null
 	ln -v "$FILE" $HOME/.vim/snippet/${FILE//*\/} 2> /dev/null
-}
+done
 
 rm -v $HOME/.vim/colors/tfl.vim 2> /dev/null
 ln -v colors/tfl.vim $HOME/.vim/colors/ 2> /dev/null
