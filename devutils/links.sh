7401fd600a8ba4616a44f8bbfb63ff80f99cd3ab
terminalforlife <terminalforlife@yahoo.com>
2021-08-01 20:46:30 +0100

    Add init.vim for Neovim to links.sh

--- devutils/links.sh
+++ devutils/links.sh
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 # Project Name      - VimConfig/devutils/links.sh
 # Started On        - Sun 22 Oct 00:15:02 BST 2017
-# Last Change       - Wed 11 Nov 17:34:35 GMT 2020
+# Last Change       - Sun  1 Aug 20:41:23 BST 2021
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
@@ -12,28 +12,31 @@
 
 {
 	if cd "$HOME/GitHub/terminalforlife/Personal/VimConfig/source"; then
-			mkdir -pv "$HOME/.vim/plugin"
-			mkdir -pv "$HOME/.vim/snippet"
-			mkdir -pv "$HOME/.vim/colors"
+		mkdir -pv "$HOME/.vim/plugin"
+		mkdir -pv "$HOME/.vim/snippet"
+		mkdir -pv "$HOME/.vim/colors"
+		mkdir -pv "$HOME/.config/nvim"
 
-			if command -v vim 1> /dev/null; then
-				for FILE in plugin/*.vim; do
-					rm -v $HOME/.vim/plugin/${FILE##*/} &&
-						ln -v "$FILE" $HOME/.vim/plugin/${FILE##*/}
-				done
+		if command -v vim 1> /dev/null; then
+			for FILE in plugin/*.vim; do
+				rm -v $HOME/.vim/plugin/${FILE##*/} &&
+					ln -v "$FILE" $HOME/.vim/plugin/${FILE##*/}
+			done
 
-				for FILE in snippet/*; do
-					rm -v $HOME/.vim/snippet/${FILE##*/} &&
-						ln -v "$FILE" $HOME/.vim/snippet/${FILE##*/}
-				done
+			for FILE in snippet/*; do
+				rm -v $HOME/.vim/snippet/${FILE##*/} &&
+					ln -v "$FILE" $HOME/.vim/snippet/${FILE##*/}
+			done
 
-				rm -v $HOME/.vim/colors/tfl.vim &&
-					ln -v colors/tfl.vim $HOME/.vim/colors/
+			rm -v $HOME/.vim/colors/tfl.vim &&
+				ln -v colors/tfl.vim $HOME/.vim/colors/
 
-				rm -v $HOME/.vim/colors/tfl-subtle.vim &&
-					ln -v colors/tfl-subtle.vim $HOME/.vim/colors/
+			rm -v $HOME/.vim/colors/tfl-subtle.vim &&
+				ln -v colors/tfl-subtle.vim $HOME/.vim/colors/
 
-				rm -v $HOME/.vimrc && ln -v .vimrc $HOME/
-			fi
+			rm -v $HOME/.vimrc && ln -v .vimrc $HOME/
+
+			rm -v $HOME/init.vim && ln -v .vimrc $HOME/.config/nvim/
+		fi
 	fi
 } 2> /dev/null
